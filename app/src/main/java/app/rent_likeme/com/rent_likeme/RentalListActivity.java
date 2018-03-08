9b98d6f6f67773824e7568446902d4bef2bfa60d
Antonio Bang <anto_004@ymail.com>
2018-03-07 18:09:07 -0800

    Using GoogleApi to get current location

--- app/src/main/java/app/rent_likeme/com/rent_likeme/RentalListActivity.java
+++ app/src/main/java/app/rent_likeme/com/rent_likeme/RentalListActivity.java
@@ -1,13 +1,29 @@
 package app.rent_likeme.com.rent_likeme;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 import app.rent_likeme.com.rent_likeme.dummy.DummyContent;
 
@@ -22,7 +38,11 @@ import app.rent_likeme.com.rent_likeme.dummy.DummyContent;
 public class RentalListActivity extends AppCompatActivity {
 
     public static final String LOG_TAG = RentalListActivity.class.getSimpleName();
+    private static final int FREQ_INTERVAL = 5000;
+    private static final int FAST_INTERVAL = 1000;
+    public static final int LOC_REQ_CODE = 200;
     private boolean mTwoPane;
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +62,9 @@ public class RentalListActivity extends AppCompatActivity {
             }
         });
 
+        checkPermissions();
+        buildGoogleApiClient();
+
         View recyclerView = findViewById(R.id.rental_list);
         assert recyclerView != null;
         setupRecyclerView((RecyclerView) recyclerView);
@@ -60,5 +83,102 @@ public class RentalListActivity extends AppCompatActivity {
     private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
         recyclerView.setAdapter(new RentalAdapter(this, mTwoPane, DummyContent.ITEMS));
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.rental_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mGoogleApiClient.disconnect();
+    }
+
+    public void getLocation() {
+        LocationRequest request = LocationRequest.create();
+        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        request.setInterval(FREQ_INTERVAL);
+        request.setFastestInterval(FAST_INTERVAL);
+
+        LocationSettingsRequest locationSettingRequest = new LocationSettingsRequest.Builder()
+                .addLocationRequest(request)
+                .build();
+        //Checking locationRequestSettings
+        SettingsClient settingsClient = LocationServices.getSettingsClient(this);
+        settingsClient.checkLocationSettings(locationSettingRequest);
+
+        FusedLocationProviderClient fusedLocationProviderClient =
+                LocationServices.getFusedLocationProviderClient(this);
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED) {
+
+            fusedLocationProviderClient.getLastLocation().addOnSuccessListener(
+                    new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    Log.v(LOG_TAG, "Got location: " + location.toString());
+                }
+            });
+        }
+
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+                    @Override
+                    public void onConnected(@Nullable Bundle bundle) {
+                        getLocation();
+                    }
+
+                    @Override
+                    public void onConnectionSuspended(int i) {
+
+                    }
+                })
+                .build();
+    }
+
+    private void checkPermissions(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION}, LOC_REQ_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case LOC_REQ_CODE: {
+                if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Your location is required", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
 }
 
